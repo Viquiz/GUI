a1d9b082295db0896288d3609f7439ea3466ab96
NguyenVux <rockmanx000@gmail.com>
2021-09-10 12:09:13 +0700

    add new sidebar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,34 +1,135 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import * as faIcons from "react-icons/fa";
-import "./navbar.css"
-// https://www.npmjs.com/package/react-pro-sidebar
+import React, { ReactNode } from "react";
+import { Link, NavLink } from "react-router-dom";
+import {ImBook} from "react-icons/im"
+import "./navbar.css";
 
-// import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-
-class NavBar extends React.Component
+interface MenuItemProps
 {
-	render() {
-		return(
-		<nav className="navbar">
-			<ul className="navlink-container border-t-2 border-b-2 border-black py-2">
-				<li>
-					<Link className="navlink" to="/Home">
-						<faIcons.FaBook className="navlink--icon"></faIcons.FaBook>
-						<span className="navlink--text"> Libraries</span>
-					</Link>
-				</li>
-
-				<li>
-					{/* <Link className="navlink" to="/devices"> */}
-						<faIcons.FaAndroid className="navlink--icon"></faIcons.FaAndroid>
-						<span className="navlink--text"> Devices</span>
-					{/* </Link> */}
-				</li>
-			</ul>
-		</nav>
-		);
+	selected:boolean;
+	icon?:ReactNode;
+	children?:ReactNode;
+	onClick:Function;
+}
+const _MenuItem = (props:MenuItemProps)=>
+{
+	function changeView() {
+		props.onClick();
 	}
+	return (
+		<li onClick={changeView} className={`flex justify-start items-center min-w-full ${props.selected?"selected":""} rounded-l-full ml-25px`}>
+			<span className={`w-50px h-50px flex justify-center items-center`}>
+				{props.icon}
+			</span>
+			<span className={`w-75px`}>
+				{props.children}
+			</span>
+		</li>
+	);
+}
+
+
+
+const _Menu =(props:any)=>
+{
+	return (
+		<ul className={`flex justify-start h-full flex-col items-start text-white`}>
+			{props.children}
+		</ul>
+
+	);
+}
+const _SideBarHeader =(props:any)=>
+{
+	return (
+		<section className={"w-full h-50px flex items-center justify-center"}>
+			{props.children}
+		</section>
+
+	);
+}
+const _SideBarContent =(props:any)=>
+{
+	return (
+		<section className={"flex-1 py-2 border-t border-b border-gray-400"}>
+			{props.children}
+		</section>
+
+	);
+}
+const _SideBarFooter =(props:any)=>
+{
+	return (
+		<section className={"w-full h-50px border-t-1 flex items-center justify-center"}>
+			{props.children}
+		</section>
+
+	);
+}
+
+const SideBar = (props:any) => {
+	return (
+		<nav className="text-white overflow-hidden w-75px bg-gray-700 hover:w-150px duration-300 flex flex-col">
+			{props.children}
+		</nav>
+	);
+}
+
+const Menu_items = [{
+	_id:0,
+	to:"/Home",
+	display:"Home",
+	icon: <ImBook/>
+},
+{
+	_id:1,
+	to:"/Setting",
+	display:"Setting",
+	icon: <ImBook/>
+},
+{
+	_id:2,
+	to:"/Setting",
+	display:"Setting",
+	icon: <ImBook/>
+},
+{
+	_id:3,
+	to:"/Setting",
+	display:"Setting",
+	icon: <ImBook/>
+},
+{
+	_id:4,
+	to:"/Setting",
+	display:"Setting",
+	icon: <ImBook/>
+},
+]
+
+
+const NavBar = () => {
+	const [view,setView] = React.useState(0);
+    return (
+	<SideBar>
+		<_SideBarHeader>
+			Viquiz
+		</_SideBarHeader>
+		<_SideBarContent>
+			<_Menu>
+				{Menu_items.map(item =>
+					<_MenuItem key={item._id} selected={view===item._id} icon={item.icon} onClick={()=>{ console.log(item._id);setView(item._id)}}>
+						<Link className="navlink" to={item.to}>
+							{item.display}
+						</Link>
+					</_MenuItem>
+				)}
+			</_Menu>
+		</_SideBarContent>
+		<_SideBarFooter>
+			FB
+		</_SideBarFooter>
+	</SideBar>
+    );
 }
 
 export default NavBar;
--- test.py
+++
@@ -1,2 +0,0 @@
-arr = [15,20,50,33,55]
-Nele = [ x for x in arr if x % 5 == 0]
