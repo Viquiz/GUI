098d37631c7fd453923b2ccb85947a67919bc122
phineasla <63248715+phineasla@users.noreply.github.com>
2022-03-14 11:51:11 +0700

    Bump dependencies
    react-router-dom remains 5.3.0 due to v6 have breaking changes

--- craco.config.js
+++ craco.config.js
@@ -1,26 +1,24 @@
 const CracoAlias = require("craco-alias");
 module.exports = {
-	style: {
-	  postcss: {
-	    plugins: [
-	      require('tailwindcss'),
-	      require('autoprefixer'),
-	    ],
-	  },
-	},plugins: [
-		{
-		  plugin: CracoAlias,
-		  options: {
-		    source: "options",
-		    baseUrl: "./",
-		    aliases: {
-		      "@components": "./src/components",
-		      "@pages": "./src/pages",
-		      "@style": "./src/style",
-		      "@common": "./src/common",
-		    },
-		    debug:false
-		  }
-		}
-	]
-}
+    style: {
+        postcssOptions: {
+            plugins: [require("tailwindcss"), require("autoprefixer")],
+        },
+    },
+    plugins: [
+        {
+            plugin: CracoAlias,
+            options: {
+                source: "options",
+                baseUrl: "./",
+                aliases: {
+                    "@components": "./src/components",
+                    "@pages": "./src/pages",
+                    "@style": "./src/style",
+                    "@common": "./src/common",
+                },
+                debug: false,
+            },
+        },
+    ],
+};
